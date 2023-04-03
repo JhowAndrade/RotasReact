7581009375d031dffc3c98e72e43c6c8821d6409
JhowAndrade <johanny_andrade@yahoo.com.br>
2023-04-03 20:52:04 -0300

    Desafio Rotas React

---
+++ src/routes/Home/HomeBody/index.tsx
@@ -0,0 +1,14 @@
+import Card from "../../../components/Card";
+
+export default function HomeBody() {
+
+    return (
+        <main>
+            <section>
+                <div className="container">
+                   <h2>Bem-Vindos!</h2> 
+                </div>
+            </section>
+        </main>
+    );
+}
---
+++ src/routes/NotFound/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "../../components/Card";
+
+export default function HomeBody() {
+
+    return (
+        <main>
+            <section>
+                <div className="container">
+                    <h2>Não encontrado!</h2>
+                </div>
+            </section>
+        </main>
+    );
+}
